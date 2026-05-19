2c7099eb344f943ad7e2fcd6c3aff9de24fea06f
Isozoda09 <ibrohimjunusov@gmail.com>
2026-05-19 17:34:43 +0500

    Fix get error in auth store

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -17,7 +17,7 @@ interface AuthState {
 
 export const useAuthStore = create<AuthState>()(
   persist(
-    (set) => ({
+    (set, get) => ({
       user: null,
       accessToken: null,
       isAuthenticated: false,
